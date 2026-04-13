558033d40b4d3bef1ffc5c7e344dba95aa08a27a
yash01dhawan-eng <yash01.dhawan@gmail.com>
2026-04-14 02:04:56 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
-        // Prints text to the console
+        // Output "Hello, World!" to the console
         System.out.println("Hello, World!");
     }
 }
